4c91b4f44e22416583f4d65cab7a8fb0ce65e1ff
Emmanuel Coulombe <emmanuelcoulombe@civalgo.com>
2024-01-15 16:34:20 -0500

    fix lint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
   },
   overrides: [
     {
